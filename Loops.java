16b5d4e03f443ec08b2d503b1ef950724eefdc5b
Shubham Gavali <shubhamgavli1711@gmail.com>
2025-10-05 17:02:56 +0530

    Loops Practice

---
+++ Loops.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Loops {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.println("Enter No: ");
+        n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
+        sc.close();
+    }
+}
