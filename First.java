3eccf8e2c41dfe244db8508561e621596043d406
Shubham Gavali <shubhamgavli1711@gmail.com>
2025-10-05 12:36:28 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+
+class First {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int a, b;
+
+    System.out.println("enter first no: ");
+    a = sc.nextInt();
+    System.out.println("enter second no: ");
+    b = sc.nextInt();
+    int sum = a + b;
+    System.out.println("sum is: " + sum);
+
+  }
+}
