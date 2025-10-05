e820181aaf4563ff1386905e4a17c56e20c49d4b
Shubham Gavali <shubhamgavli1711@gmail.com>
2025-10-05 14:16:11 +0530

    Switch Practice

---
+++ Switch.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Switch{
+    public static void main(String args[]){
+        int num;
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter any no(1-7): ");
+        num=sc.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        
+            default:
+                System.out.println("Enter valid number");
+                break;
+        }
+    }
+}
