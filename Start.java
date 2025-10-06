27bf84d7cc7ff4f7e23ceab56f60d6ac67a4c79c
Shubham Gavali <shubhamgavli1711@gmail.com>
2025-10-06 23:54:30 +0530

    sum

---
+++ Start.java
@@ -0,0 +1,8 @@
+public class Start {
+    public static void main(String args[]){
+        int x=10;
+        int y=10;
+        int sum = x+y;
+        System.out.println("sum is " + sum);
+    }
+}
