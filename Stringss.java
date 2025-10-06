d3b041bf4912fc5ec5e04cfcd7d09e555e52aa6a
Shubham Gavali <shubhamgavli1711@gmail.com>
2025-10-06 23:34:13 +0530

    string methood practice

---
+++ Stringss.java
@@ -0,0 +1,13 @@
+
+
+import java.lang.*;
+import java.util.*;
+
+class Stringss {
+    public static void main(String args[]) {
+        String str = "Hello World";
+        String str1 =str.substring(6);
+        String str2 =str.substring(0,6);
+        System.out.println(str1 + str2);
+    }
+}
