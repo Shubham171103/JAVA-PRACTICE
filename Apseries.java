a9d093b86e752e7e071f4bd9a02577d8cea687dd
Shubham Gavali <shubhamgavli1711@gmail.com>
2025-10-07 00:29:20 +0530

    Apseries

---
+++ Apseries.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Apseries{
+    public static void main(String args[]){
+        int a,n,d;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no of terms: ");
+        n=sc.nextInt();
+        System.out.println("Enter First Term: ");
+        a=sc.nextInt();
+        
+    }
+}
